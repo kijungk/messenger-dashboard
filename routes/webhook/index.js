923aae064511e9966f2b7351970e37696797f99e
Ki Jung Kim <kijungkim@eworldes.com>
2019-07-10 11:59:07 -1000

    modified webhook endpoint to postbacks

--- routes/webhook/index.js
+++ routes/webhook/index.js
@@ -1,8 +1,8 @@
 const
   express = require('express'),
-//   constants = require('../../utilities/constants'),
-//   eventHandler = require('../../utilities/handlers/eventHandler'),
-//   sendHandler = require('../../utilities/handlers/sendHandler'),
+  { httpStatusCodes } = require('../../utilities/constants'),
+  eventHandler = require('../../utilities/handlers/eventHandler'),
+  sendHandler = require('../../utilities/handlers/sendHandler'),
   router = express.Router();
 
 router.route('/')
@@ -15,52 +15,53 @@ router.route('/')
       challenge = request.query['hub.challenge'];
 
     if (!mode || !token) {
-      return response.status(constants.httpStatusCodes.badRequest).json({
+      return response.status(httpStatusCodes.badRequest).json({
         message: 'Token verification has failed. Please input a mode and a token.'
       });
     }
 
     if (mode !== 'subscribe' || token !== VERIFY_TOKEN) {
-      return response.status(constants.httpStatusCodes.forbidden).json({
+      return response.status(httpStatusCodes.forbidden).json({
         message: 'Token verification has failed. Please check your mode and token.'
       });
     }
 
-    return response.status(200).send(challenge);
+    return response.status(httpStatusCodes.ok).send(challenge);
   })
 
-//   .post((request, response) => {
-//     const body = request.body;
+  .post((request, response) => {
+    const body = request.body;
 
-//     if (body.object !== 'page') {
-//       return response.status(404).json({
-//         message: 'POST request to /webhook has failed. Please check that the event is from a page subscription.'
-//       });
-//     }
+    if (body.object !== 'page') {
+      // TODO error log
+      return response.status(httpStatusCodes.notFound).json({
+        message: 'POST request to /webhook has failed. Please check that the event is from a page subscription.'
+      });
+    }
 
-//     body.entry.forEach((entry) => {
-//       const event = entry.messaging[0];
-//       const senderId = event.sender.id;
-//       let payload = '';
+    body.entry.forEach((entry) => {
+      const event = entry.messaging[0];
+      const senderId = event.sender.id;
+      let payload = '';
 
-//       // entry has page id which can be differentiated to respond to multiple pages
+      // entry has page id which can be differentiated to respond to multiple pages
 
-//       if (event.message) {
-//         // differentiate between user inputs and assign payload here
-//         payload = event.message.text;
-//       }
+      if (event.message) {
+        // differentiate between user inputs and assign payload here
+        payload = event.message.text;
+      }
 
-//       if (event.postback) {
-//         payload = event.postback.payload;
-//       }
+      if (event.postback) {
+        payload = event.postback.payload;
+      }
 
-//       const message = eventHandler.processPayload(payload);
+      const message = eventHandler.processPayload(payload);
 
-//       return sendHandler.send(senderId, message);
-//     });
+      return sendHandler.send(senderId, message);
+    });
 
-//     return response.status(200).json({
-//       message: 'Event successfully received.'
-//     });
-//   })
+    return response.status(httpStatusCodes.ok).json({
+      message: 'Event successfully received.'
+    });
+  })
 module.exports = router;
