d7d8ab285add92b6af60d8e32b6b03479345be34
Ki Jung Kim <kijungkim@eworldes.com>
2019-07-16 11:45:19 -1000

    testing differentiation of requests

--- utilities/handlers/responseHandler.js
+++ utilities/handlers/responseHandler.js
@@ -217,8 +217,8 @@ module.exports = (function responseHandler() {
       case 'Home':
         buttons = [
           new Button('Agenda', 'postback', 'Agenda'),
-          new Button('Experience', 'postback', 'Experience'),
-          new Button('General Information', 'postback', 'GeneralInformation')
+          new Button('General Information', 'postback', 'GeneralInformation'),
+          new Button('Survey', 'postback', 'Experience')
         ];
 
         elements = [
@@ -231,108 +231,19 @@ module.exports = (function responseHandler() {
 
       case 'Agenda':
         elements = [
-          new Element('Agenda Item 1', '9:00 am - 10:00 am', 'https://via.placeholder.com/1910x1000'),
-          new Element('Agenda Item 2', '10:00 am - 11:00 am', 'https://via.placeholder.com/1910x1000'),
-          new Element('Agenda Item 3', '11:00 am - 12:00 pm', 'https://via.placeholder.com/1910x1000'),
+          new Element('OxC Item 1', '9:00 am - 10:00 am', 'https://via.placeholder.com/1910x1000'),
+          new Element('OxC Item 2', '10:00 am - 11:00 am', 'https://via.placeholder.com/1910x1000'),
+          new Element('OxC Item 3', '11:00 am - 12:00 pm', 'https://via.placeholder.com/1910x1000'),
           new Element('Lunch', '12:00 pm - 1:00 pm', 'https://via.placeholder.com/1910x1000'),
-          new Element('Agenda Item 5', '1:00 pm - 2:00 pm', 'https://via.placeholder.com/1910x1000'),
-          new Element('Agenda Item 6', '2:00 pm - 3:00 pm', 'https://via.placeholder.com/1910x1000'),
-          new Element('Agenda Item 7', '3:00 pm - 4:00 pm', 'https://via.placeholder.com/1910x1000'),
-          new Element('Agenda Item 8', '4:00 pm - 5:00 pm', 'https://via.placeholder.com/1910x1000')
+          new Element('OxC Item 5', '1:00 pm - 2:00 pm', 'https://via.placeholder.com/1910x1000'),
+          new Element('OxC Item 6', '2:00 pm - 3:00 pm', 'https://via.placeholder.com/1910x1000'),
+          new Element('OxC Item 7', '3:00 pm - 4:00 pm', 'https://via.placeholder.com/1910x1000'),
+          new Element('OxC Item 8', '4:00 pm - 5:00 pm', 'https://via.placeholder.com/1910x1000')
         ];
 
         attachment = new Attachment('generic', elements);
 
         quickReplies = [new QuickReply('Back', 'Home')];
-
-        break;
-
-      case 'Experience':
-        buttons = [
-          new Button('Booths', 'postback', 'Booths')
-        ];
-
-        elements = [
-          new Element('Experience and Learn', 'Earn SWAG using QR codes at each booth', 'https://via.placeholder.com/1910x1000', buttons)
-        ];
-
-        attachment = new Attachment('generic', elements);
-
-        quickReplies = [new QuickReply('Back', 'Home')];
-
-        break;
-
-      case 'Booths':
-        elements = [
-          new Element('Booth 1', 'Booth 1 details', 'https://via.placeholder.com/1910x1000'),
-          new Element('Booth 2', 'Booth 2 details', 'https://via.placeholder.com/1910x1000'),
-          new Element('Booth 3', 'Booth 3 details', 'https://via.placeholder.com/1910x1000'),
-          new Element('Booth 4', 'Booth 4 details', 'https://via.placeholder.com/1910x1000'),
-          new Element('Booth 5', 'Booth 5 details', 'https://via.placeholder.com/1910x1000'),
-          new Element('Booth 6', 'Booth 6 details', 'https://via.placeholder.com/1910x1000'),
-          new Element('Booth 7', 'Booth 7 details', 'https://via.placeholder.com/1910x1000'),
-          new Element('Booth 8', 'Booth 8 details', 'https://via.placeholder.com/1910x1000')
-        ];
-
-        attachment = new Attachment('generic', elements);
-
-        quickReplies = [new QuickReply('Back', 'Experience'), new QuickReply('Home', 'Home')];
-
-        break;
-
-      case 'BoothOneComplete':
-        attachment = 'You completed Booth 1 scavenger hunt!\n\nFeel free to see what\'s happening at this booth, or check out the other booths available.';
-
-        quickReplies = [new QuickReply('Booths', 'Booths'), new QuickReply('Home', 'Home')];
-
-        break;
-
-      case 'BoothTwoComplete':
-        attachment = 'You completed Booth 2 scavenger hunt!\n\nFeel free to see what\'s happening at this booth, or check out the other booths available.';
-
-        quickReplies = [new QuickReply('Booths', 'Booths'), new QuickReply('Home', 'Home')];
-
-        break;
-
-      case 'BoothThreeComplete':
-        attachment = 'You completed Booth 3 scavenger hunt!\n\nFeel free to see what\'s happening at this booth, or check out the other booths available.';
-
-        quickReplies = [new QuickReply('Booths', 'Booths'), new QuickReply('Home', 'Home')];
-
-        break;
-
-      case 'BoothFourComplete':
-        attachment = 'You completed Booth 4 scavenger hunt!\n\nFeel free to see what\'s happening at this booth, or check out the other booths available.';
-
-        quickReplies = [new QuickReply('Booths', 'Booths'), new QuickReply('Home', 'Home')];
-
-        break;
-
-      case 'BoothFiveComplete':
-        attachment = 'You completed Booth 5 scavenger hunt!\n\nFeel free to see what\'s happening at this booth, or check out the other booths available.';
-
-        quickReplies = [new QuickReply('Booths', 'Booths'), new QuickReply('Home', 'Home')];
-
-        break;
-
-      case 'BoothSixComplete':
-        attachment = 'You completed Booth 6 scavenger hunt!\n\nFeel free to see what\'s happening at this booth, or check out the other booths available.';
-
-        quickReplies = [new QuickReply('Booths', 'Booths'), new QuickReply('Home', 'Home')];
-
-        break;
-
-      case 'BoothSevenComplete':
-        attachment = 'You completed Booth 7 scavenger hunt!\n\nFeel free to see what\'s happening at this booth, or check out the other booths available.';
-
-        quickReplies = [new QuickReply('Booths', 'Booths'), new QuickReply('Home', 'Home')];
-
-        break;
-
-      case 'BoothEightComplete':
-        attachment = 'You completed Booth 8 scavenger hunt!\n\nFeel free to see what\'s happening at this booth, or check out the other booths available.';
-
-        quickReplies = [new QuickReply('Booths', 'Booths'), new QuickReply('Home', 'Home')];
 
         break;
 
@@ -354,9 +265,9 @@ module.exports = (function responseHandler() {
         break;
 
       case 'Venue':
-        buttons = [new Button('Website', 'web_url', 'https://www.ddp.or.kr/main')];
+        buttons = [new Button('Website', 'web_url', 'https://www.shilla.net/seoul/index.do')];
 
-        attachment = new Attachment('button', buttons, 'This demo event will be held at Dongdaemun Design Plaza (DDP).\n\nYou can find more about the venue on its website!')
+        attachment = new Attachment('button', buttons, 'This demo event will be held at Seoul Shilla Hotel.\n\nYou can find more about the venue on its website!')
 
         quickReplies = [
           new QuickReply('Back', 'GeneralInformation'),
