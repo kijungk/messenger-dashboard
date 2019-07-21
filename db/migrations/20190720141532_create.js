a8a6c0c2bcbbc08c22674158237880e6a7e09852
Ki Jung Kim <kijungkim@eworldes.com>
2019-07-20 14:21:10 -1000

    made migrations for users

--- db/migrations/20190720141532_create.js
+++ db/migrations/20190720141532_create.js
@@ -2,6 +2,7 @@
 exports.up = function(knex) {
   return knex.schema.createTable('users', (table) => {
     table.increments();
+    table.string('name', 256).notNullable();
     table.string('facebook_id', 256).notNullable().unique();
     table.timestamps(true, true);
   });
