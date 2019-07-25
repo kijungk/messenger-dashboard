883b38228404a97676abb1c690f5d1a9d4584d64
Ki Jung Kim <kijungkim@eworldes.com>
2019-07-24 21:25:35 -1000

    adding frontend into application

--- routes/events/index.js
+++ routes/events/index.js
@@ -16,14 +16,37 @@ router.route('/')
           icons
           ON icons.id = events.icon_id
       `)
-      .then((query) => {
-        const { rows } = query;
+      .then((result) => {
+        const { rows } = result;
         return response.status(200).send(rows);
       })
       .catch((error) => {
         console.log(error);
         // error while fetching all events
       });
+  });
+
+router.route('/:id')
+  .get((request, response) => {
+    const id = request.params.id;
+
+    return knex.raw(`
+      SELECT
+        events.id,
+        events.description,
+        icons.url
+      FROM
+        events
+      JOIN
+        icons
+        ON icons.id = events.icon_id
+    `)
+      .then((result) => {
+        const { rows } = result;
+        const row = rows[0];
+
+        return response.status(200).send(row);
+      })
   })
 
 module.exports = router;
