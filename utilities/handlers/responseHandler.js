439e9f020373984da3ec11294a4c424ea03f0cfc
Ki Jung Kim <kijungkim@eworldes.com>
2019-08-06 16:23:58 -1000

    adding in orders stream

--- utilities/handlers/responseHandler.js
+++ utilities/handlers/responseHandler.js
@@ -28,7 +28,7 @@ module.exports = (function responseHandler() {
       WHERE
         p.description = :productDescription
       RETURNING
-        id, complete, p.description
+        id, complete
     `, {
         eventDescription,
         productDescription
