9fbcac3ccb2d9beaa19db31bdecec92aaa49eb67
Ki Jung Kim <kijungkim@eworldes.com>
2019-08-11 12:11:52 -1000

    Adding redis

--- dist/messenger-dashboard-frontend/main.js
+++ dist/messenger-dashboard-frontend/main.js
@@ -604,7 +604,7 @@ var OrdersComponent = /** @class */ (function () {
 /*! no static exports found */
 /***/ (function(module, exports) {
 
-module.exports = "<div class=\"page\">\n  <div class=\"page-container\">\n    <div id=\"events\">\n      <div *ngIf=\"event | async as event\" id=\"event\">\n        <img src={{event.url}} alt={{event.description}} id=\"event-image\">\n        <div id=\"title-container\">\n          <div class=\"oswald\">\n            {{event.description}}\n          </div>\n          <div class=\"roboto\">\n            Dashboard\n          </div>\n        </div>\n      </div>\n      <div id=\"menu\">\n        <div class=\"card\" (click)=\"open('broadcast')\">\n          <i class=\"fas fa-bullhorn\"></i>\n          <div class=\"oswald\">\n            Push Notification\n          </div>\n        </div>\n        <div class=\"card\" *ngIf=\"controller.ordersMenu\" (click)=\"open('orders')\">\n          <i class=\"fas fa-cash-register\"></i>\n          <div class=\"oswald\">\n            Orders\n          </div>\n        </div>\n      </div>\n    </div>\n  </div>\n  <app-orders *ngIf=\"controller.orders\"></app-orders>\n  <app-broadcast *ngIf=\"controller.broadcast\"></app-broadcast>\n</div>"
+module.exports = "<div class=\"page\">\n  <div class=\"page-container\">\n    <div id=\"events\">\n      <div *ngIf=\"event | async as event\" id=\"event\">\n        <img src={{event.url}} alt={{event.description}} id=\"event-image\">\n        <div id=\"title-container\">\n          <div class=\"oswald\">\n            {{event.description}}\n          </div>\n          <div class=\"roboto\">\n            Dashboard\n          </div>\n        </div>\n      </div>\n      <div id=\"menu\">\n        <div class=\"card\" (click)=\"open('broadcast')\">\n          <i class=\"fas fa-bullhorn\"></i>\n          <div class=\"oswald\">\n            Push Notification\n          </div>\n        </div>\n        <div class=\"card\" *ngIf=\"menuController.ordersMenu\" (click)=\"open('orders')\">\n          <i class=\"fas fa-cash-register\"></i>\n          <div class=\"oswald\">\n            Orders\n          </div>\n        </div>\n      </div>\n    </div>\n  </div>\n  <app-orders *ngIf=\"controller.orders\"></app-orders>\n  <app-broadcast *ngIf=\"controller.broadcast\"></app-broadcast>\n</div>"
 
 /***/ }),
 
@@ -647,15 +647,17 @@ var EventsComponent = /** @class */ (function () {
         this.controller = {
             broadcast: false,
             orders: false,
+        };
+        this.menuController = {
             ordersMenu: false
         };
     }
     EventsComponent.prototype.ngOnInit = function () {
         this.id = this.route.snapshot.params.id;
-        if (Number(this.id) === 1) {
-            this.controller['ordersMenu'] = true;
-        }
         this.controller = this.modalsService.controller;
+        if (Number(this.id) === 1) {
+            this.menuController['ordersMenu'] = true;
+        }
         this.event = this.getEvent(this.id);
     };
     EventsComponent.prototype.getEvent = function (id) {
