e67c6a4d39eba42a5a71723dab86429724f8d9dd
Ki Jung Kim <kijungkim@eworldes.com>
2019-08-11 09:57:12 -1000

    remodeling permissions

--- db/migrations/20190811095105_alter.js
+++ db/migrations/20190811095105_alter.js
@@ -1,6 +1,8 @@
 
 exports.up = function(knex) {
   return knex.schema.table('permissions', (table) => {
+    table.dropColumn('description');
+    table.string('description').notNullable();
     table.integer('vendor_id');
     table.foreign('vendor_id').references('id').inTable('vendors');
   });
