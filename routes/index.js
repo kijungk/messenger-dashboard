a737871b307f76e81d24a9e606ddfb0573beabaf
Ki Jung Kim <kijungkim@eworldes.com>
2019-08-10 20:57:33 -1000

    adding in altdif menu

--- routes/index.js
+++ routes/index.js
@@ -5,10 +5,12 @@ const
 const
   events = require('./events'),
   orders = require('./orders'),
+  users = require('./users'),
   webhook = require('./webhook');
 
 router.use('/events', events);
 router.use('/orders', orders);
+router.use('/users', users);
 router.use('/webhook', webhook);
 
 module.exports = router;
