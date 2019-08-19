8790346c2768d0266f23df46872d6560c876af88
Ki Jung Kim <kijungkim@eworldes.com>
2019-08-19 13:23:05 -1000

    adding inventory changing ui

--- routes/index.js
+++ routes/index.js
@@ -6,11 +6,13 @@ const
   broadcasts = require('./broadcasts'),
   events = require('./events'),
   orders = require('./orders'),
+  products = require('./products'),
   webhook = require('./webhook');
 
 router.use('/broadcasts', broadcasts);
 router.use('/events', events);
 router.use('/orders', orders);
+router.use('/products', products);
 router.use('/webhook', webhook);
 
 module.exports = router;
