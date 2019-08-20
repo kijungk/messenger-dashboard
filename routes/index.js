9a76490363ea039532fac29768f39b3547e4f045
Ki Jung Kim <kijungkim@eworldes.com>
2019-08-20 12:32:10 -1000

    adding in frontend locking system

--- routes/index.js
+++ routes/index.js
@@ -4,12 +4,14 @@ const
 
 const
   broadcasts = require('./broadcasts'),
+  controllers = require('./controllers'),
   events = require('./events'),
   orders = require('./orders'),
   products = require('./products'),
   webhook = require('./webhook');
 
 router.use('/broadcasts', broadcasts);
+router.use('/controllers', controllers);
 router.use('/events', events);
 router.use('/orders', orders);
 router.use('/products', products);
